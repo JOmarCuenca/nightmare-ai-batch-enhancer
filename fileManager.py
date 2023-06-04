ef06708f1f3c81ecc183944ca118844de80231c5
Omar Cuenca <jesomar.cuenca@gmail.com>
2023-06-03 20:55:49 -0600

    Fix name generator

--- fileManager.py
+++ fileManager.py
@@ -4,21 +4,22 @@ import os
 class FileManager:
 
     __VALID_EXTENSIONS = {".jpg", ".jpeg", ".png", ".webp"}
+    counter = 1
 
-    def __init__(self, outputPath):
-        self.outputPath = outputPath
+    def __init__(self, path):
+        self.path = path
 
     def isDir(self):
-        return os.path.isdir(self.outputPath)
+        return os.path.isdir(self.path)
 
     def isFile(self):
-        return os.path.isfile(self.outputPath)
+        return os.path.isfile(self.path)
 
     def exists(self):
-        return os.path.exists(self.outputPath)
+        return os.path.exists(self.path)
 
     def getName(self):
-        return os.path.splitext(self.outputPath)[0]
+        return os.path.splitext(self.path)[0]
 
     def getExtension(path: str):
         return os.path.splitext(path)[1]
@@ -28,24 +29,24 @@ class FileManager:
         return extension in FileManager.__VALID_EXTENSIONS
 
     def getNextValidName(self, extension):
-        counter = 1
+        
 
         while True:
             path = ""
             if (self.isDir()):
                 path = os.path.join(
-                    self.getName(), f"output_img_{counter}{extension}")
+                    self.getName(), f"output_img_{self.counter}{extension}")
             else:
-                path = f"{self.getName()}_{counter}{extension}"
+                path = f"{self.getName()}_{self.counter}{extension}"
 
             if (not os.path.exists(path)):
-                yield path
+                return path
             else:
-                counter += 1
+                self.counter += 1
 
     def getImagesDirs(self):
         assert self.isDir(), "Path is not a directory"
-        return [os.path.join(self.outputPath, f) for f in os.listdir(self.outputPath) if FileManager.isValidExtension(f)]
+        return [os.path.join(self.path, f) for f in os.listdir(self.path) if FileManager.isValidExtension(f)]
 
 
 if __name__ == '__main__':
